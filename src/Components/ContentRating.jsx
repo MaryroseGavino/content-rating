9c596126f8b7353b1ec69f3c3ad14b015505f3c4
Mary Rose Gavino <gavinomaryrose360@gmail.com>
2025-09-24 13:42:47 +0800

    delete the h1 tag / try commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,17 +1,12 @@
-
-import React, { Component } from 'react';
-import './ContentRating.css';
+import React, { Component } from "react";
+import "./ContentRating.css";
 
 class ContentRating extends Component {
   constructor() {
     super();
   }
   render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
+    return <div></div>;
   }
 }
 
