96268f2f3046033b21459538d5bd2177e6024fbf
Mary Rose Gavino <gavinomaryrose360@gmail.com>
2025-09-24 13:48:47 +0800

    added random qoute for the p tag

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,9 +5,9 @@ class ContentRating extends Component {
   constructor() {
     super();
     this.state = {
-      likes: 0, 
+      likes: 0,
       dislikes: 0,
-       handleLikes: () => {
+      handleLikes: () => {
         this.setState((prevState) => ({
           likes: prevState.likes + 1,
         }));
@@ -17,10 +17,19 @@ class ContentRating extends Component {
           dislikes: prevState.dislikes + 1,
         }));
       },
-    }
+    };
   }
   render() {
-    return <div></div>;
+    return (
+      <div className="content-rating">
+        <p>
+          The two most important days in your life are the day you are born and
+          the day you find out why.
+        </p>
+        <strong>- Mark Twain</strong>
+        
+      </div>
+    );
   }
 }
 
